cd1ff07b826d77caa8179a4163e2864b6261a016
christianazu <christianazu2@icloud.com>
2022-10-02 19:54:52 -0300

    mod4

--- forms.js
+++ forms.js
@@ -1,34 +1,22 @@
 
-const init = function(){
+const init = function () {
     document.getElementById('button-cancel').addEventListener('click', reset);
     document.getElementById('button-send').addEventListener('click', send);
 }
 
-const reset = function(ev){
-    //HTML will automatically put the form back to its initial state
-    //unless we do 
+const reset = function (ev) {
     ev.preventDefault();
-    // programmatically we can reset it 
     document.getElementById('form-user').reset();
-    //if you want to do anything else...
 }
 
-const send = function(ev){
-    ev.preventDefault(); 
+const send = function (ev) {
+    ev.preventDefault();
     ev.stopPropagation();
-    //or the click will travel to the form and the form will submit
     let fails = validate();
-    //IF we wanted to do some async things then use a Promise with .then and .catch
-    if(fails.length === 0){
-        //good to go
+    if (fails.length === 0) {
         document.getElementById('form-user').submit();
-    }else{
-        //there are some errors to display
-        //bad user
-        //let err = document.querySelector('.error');
-        //let input = err.querySelector('input');
-        //err.setAttribute('data-errormsg', ` ... Missing ${input.placeholder}`);
-        fails.forEach(function(obj){
+    } else {
+        fails.forEach(function (obj) {
             let field = document.getElementById(obj.input);
             field.parentElement.classList.add('error');
             field.parentElement.setAttribute('data-errormsg', obj.msg);
@@ -36,35 +24,27 @@ const send = function(ev){
     }
 }
 
-const validate = function(ev){
-    //let valid = true;
+const validate = function (ev) {
     let failures = [];
- 
-    // //select
-    // let select = document.getElementById('input-age');
-    // // .selectedIndex  .options  .length   .selectedValue  .value
-    // if( select.selectedIndex === 0 ){
-    //     failures.push({input:'input-age', msg:'Too young'})
-    // }
-
-    //inputs for text, email, tel, color, number...
-    let first = document.getElementById('input-name');
-    //let password = document.getElementById('input-password');
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    let name = document.getElementById('input-name');
+    let subject = document.getElementById('input-subject');
     let email = document.getElementById('input-email');
-    //.value, .defaultValue, length of value
-    if( first.value === ""){
-        failures.push({input:'input-name', msg:'Required Field'})
-    } 
-//    if( password.value === "" || password.value.length < 8){
-//        failures.push({input:'input-password', msg:'Must be at least 8 chars'})
-//    } 
-    if( email.value === ""){
-        failures.push({input:'input-email', msg:'Required Field'})
+
+    if (name.value === "") {
+        failures.push({ input: 'input-name', msg: 'Complete nombre' })
     }
-    
-    //return a boolean || an object with details about the failures
+    if (subject.value === "") {
+        failures.push({ input: 'input-subject', msg: 'Complete Asunto' })
+    }
+    if (email.value === "") {
+        failures.push({ input: 'input-email', msg: 'Complete email' })
+    }
+
+    if (reg.test(email.value) == false) {
+        failures.push({ input: 'input-email', msg: 'Complete con formato email' })
+    }
     return failures;
 }
-
 
 document.addEventListener('DOMContentLoaded', init);
